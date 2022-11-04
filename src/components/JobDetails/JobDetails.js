df66ba56ee3b0661f10e7265f9df717ae86add7d
jokerav <jokeravdeveloper@gmail.com>
2022-11-04 17:38:48 +0200

    Update JobDetails.js

--- src/components/JobDetails/JobDetails.js
+++ src/components/JobDetails/JobDetails.js
@@ -6,16 +6,14 @@ const JobDetails = ()=>{
     // , error, isLoading
   } = useGetDataQuery();
   const {jobId} = useParams();
-
-  const currentJob = data.filter(job => job.id === jobId);
+  const [currentJob] = data.filter(job => job.id === jobId);
   console.log(currentJob);
+  const {description} = currentJob;
   return(
     <>
 {<Link to='/'>Back</Link>}
       <p>{jobId}</p>
-      </>
-
-
+      <p>{description}</p></>
   )
 }
 export default JobDetails;
