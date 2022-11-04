7d30cf09851989d51a89efcc745952aa1ec4841f
jokerav <jokeravdeveloper@gmail.com>
2022-11-04 19:18:35 +0200

    rating

--- src/components/jobItem/JobItem.js
+++ src/components/jobItem/JobItem.js
@@ -1,5 +1,14 @@
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
+import { Rating } from 'react-simple-star-rating'
+
 const JobItem = ({ job })=> {
+  const [rating, setRating] = useState(0)
+  const handleRating = (rate: number) => {
+    setRating(rate)
+
+    // other logic
+  }
   const {title, id, createdAt, pictures, address} = job;
   console.log(job)
 
@@ -9,7 +18,12 @@ const JobItem = ({ job })=> {
       <p>{createdAt}</p>
       <img src={pictures[0]} alt="Job title"></img>
       <p>{address}</p>
-      </Link>
+    </Link>
+      <Rating
+        onClick={handleRating}
+        /* Available Props */
+      />
+
   </li>
 }
 export default JobItem
