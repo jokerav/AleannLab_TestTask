050cf533ecd101ec20b10add09cba677f1b8ea36
jokerav <jokeravdeveloper@gmail.com>
2022-11-04 20:28:06 +0200

    правки

--- src/components/jobItem/JobItem.js
+++ src/components/jobItem/JobItem.js
@@ -1,23 +1,27 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import { Rating } from 'react-simple-star-rating'
+import { differenceInMonths } from 'date-fns'
+import {ReactComponent as BookMark} from '../../img/Rectangle 31.svg';
+import {ReactComponent as MapStamp} from '../../img/mapStamp.svg';
 
 const JobItem = ({ job })=> {
-  const [rating, setRating] = useState(0)
+  const [, setRating] = useState(0)
   const handleRating = (rate: number) => {
     setRating(rate)
-
     // other logic
   }
   const {title, id, createdAt, pictures, address} = job;
-  console.log(job)
-
+  const difference = differenceInMonths(new Date, new Date(createdAt));
   return <li>
     <Link to={`/job/${id}`}>
       <p>{title}</p>
       <p>{createdAt}</p>
       <img src={pictures[0]} alt="Job title"></img>
       <p>{address}</p>
+      <BookMark/>
+      <p>Posted {difference} month ago</p>
+      <MapStamp height={13}/>
     </Link>
       <Rating
         onClick={handleRating}
